5b6106e4bc50b656175b703e72de7042c8728be6
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-26 23:34:30 +0200

    home navigation done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,52 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
-import axios from "axios";
+
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="body flex column center rubik-regular">
       <nav className="flex row nav-bar black-text-p">
         <div><i className="icon"></i></div>
         <div className="flex row nav-bar-anchors">
-          <button>Home</button>
-          <button>Weather</button>
-          <button>Calculator</button>
-          <button>Sticky Notes</button>
+          <button onClick={()=>{
+            navigate('/')
+          }}>Home</button>
+          <button onClick={()=>{
+            navigate('/Weather')
+          }}>Weather</button>
+          <button onClick={()=>{
+            navigate('/Calculator')
+          }}>Calculator</button>
+          <button onClick={()=>{
+            navigate('/StickyNotes')
+          }}>Sticky Notes</button>
         </div>
       </nav>
+
       <div className="header flex center">
         <h1>Welcome to MULTI-APP</h1>
       </div>
+
       <section className="Nav-cards flex column center">
         <div className="Nav-card landing-sec cover-1 rounded">
           <div className="centered">
             <h2>Weather App</h2>
             <p>A weather app displays current weather conditions and forecasts for locations, helping users plan their day.</p>
-            <button>Check it Out!</button>
+            <button onClick={()=>{
+            navigate('/Weather')
+          }}>Check it Out!</button>
           </div>
         </div>
         <div className="Nav-card landing-sec cover-2 rounded">
           <div className="centered-2">
             <h2>Calculator App</h2>
             <p>A calculator app performs basic and advanced mathematical calculations quickly and accurately.</p>
-            <button>Check it Out!</button>
+            <button onClick={()=>{
+            navigate('/Calculator')
+          }}>Check it Out!</button>
           </div>
           
         </div>
@@ -37,7 +54,9 @@ const Home = () => {
           <div className="centered-3">
             <h2>Sticky Notes App</h2>
             <p>A sticky notes app creates digital notes for reminders, to-do lists, or short notes, aiding in organization.</p>
-            <button>Check it Out!</button>
+            <button onClick={()=>{
+            navigate('/StickyNotes')
+          }}>Check it Out!</button>
           </div>
         </div>
       </section>
